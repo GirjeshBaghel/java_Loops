e4d89fad3b0801390058e783be1a4c3cd864fd30
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-16 19:14:19 +0530

    Add files via upload

---
+++ Pyramid_Reverse.java
@@ -0,0 +1,20 @@
+class Pyramid_Reverse 
+{
+
+	public static void main(String[] args) {
+    int i,j,k;
+    for( i=5;i>=1;i--) 
+    {
+    	     for(k=4;k>=i;k--)//k for space 
+    	     {
+    	    	 System.out.print(" ");
+    	     }
+    	     for( j=1;j<=2*i-1;j++) {
+    	    	 System.out.print("*");
+      	     }
+                 System.out.println();
+    }
+    
+	}
+
+}
---
+++ Sum_Of_Digit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Sum_Of_Digit 
+{
+	
+	public static void main ( String args[])
+	{
+		int n,res,sum=0;
+		Scanner sc= new Scanner( System.in);
+		System.out.println( " Enter the number : ");
+		n=sc.nextInt();	
+		do
+		{
+			res=n%10;
+			sum=sum+res;
+			n=n/10;
+		}
+		while( n>0);
+		System.out.println("The sum of digits is = "+sum);
+		
+	}
+
+ }
