f0c2eacf19b4a83327fbca50961f03fbd3ab1fee
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-18 19:06:09 +0530

    Update G_Pattern.java

--- G_Pattern.java
+++ G_Pattern.java
@@ -1,22 +1,29 @@
 class G_Pattern
 {
+    //intilize the class variable 
     static int height = 5;
     static int width = (2 * height) - 1;
 
+    //main method
     public static void main(String[] args)
     {
+        //printG function is calling inside the main method
         printG();
     }
-
-    static void printG()
+    
+    // this is printG method it is static method 
+    public static void printG()
     {
     int i, j;
     width--;
     
+     // it is for loop running by the height
     for (i = 0; i < height; i++)
     {
+        // it is for loop running by the width
         for (j = 0; j < width; j++)
         {
+            // conditions checking and print according the conditions 
             if ((i == 0 || i == height - 1) && (j == 0 || j == width - 1))
             System.out.printf(" ");
             
@@ -41,6 +48,8 @@ class G_Pattern
             else
                 System.out.printf(" ");
         }
+        
+        // it is used for break the line
         System.out.printf("\n");
     }
 }
