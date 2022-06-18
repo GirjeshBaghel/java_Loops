ce28fc2911a7244e1ba8a5f72d8d55866c10274c
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-18 18:59:15 +0530

    Update Binary_No.java

--- Binary_No.java
+++ Binary_No.java
@@ -1,11 +1,18 @@
  class Binary_No 
 {
+	 //main method
 	public static void main (String args[]) {
+		
+		//initiliazing the variable 
 		int i,j;
+		
+		//for loop running
 		for( i=1;i<=5;i++)
 		{
 			     for( j=5;j>=i;j--)
 			     {
+				    
+				    //chacking conditions 
 			    	   if( j%2==0) {
 			    		   System.out.print( "0"); 
 			    	   }
