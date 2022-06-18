46b06279f43875692a8763710e670ac6fae57070
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-18 19:08:06 +0530

    Update Left_Star_Pattern.java

--- Left_Star_Pattern.java
+++ Left_Star_Pattern.java
@@ -1,13 +1,19 @@
 public class Left_Star_Pattern {
-
+	
+	// this main method
 	public static void main(String[] args) {
+		
+		// it is local variable
 		int i,j;
+		
+		// it is for_loop for print a star pattern
 		for(i=1;i<=5;i++ ) 
 		{
 			for(j=1;j<=i;j++ ) 
 			{
 				System.out.print("*" );
 			}
+			   // this line is used for braking the line
 			   System.out.println();
 		}
 
