6fa0a7501f157b5434f4b86c9e5f7d3ba455a0d6
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-18 20:50:57 +0530

    Update Pyramid_Reverse.java

--- Pyramid_Reverse.java
+++ Pyramid_Reverse.java
@@ -1,10 +1,11 @@
 class Pyramid_Reverse 
 {
-
+	// main method
 	public static void main(String[] args) {
     int i,j,k;
     for( i=5;i>=1;i--) 
-    {
+    {		
+	    // k loop for space
     	     for(k=4;k>=i;k--)//k for space 
     	     {
     	    	 System.out.print(" ");
