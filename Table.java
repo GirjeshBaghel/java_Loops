a9cbad9bc09a36ab06a3eede2e7b3fe08cce13af
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-18 20:53:11 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,9 +1,12 @@
 import java.util.*;
 class Table
 {
+    // main method
     public static void main(String[] args)
     {
         int a;
+        
+        // taking no from user to print a table
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the no : ");
         a = sc.nextInt();
